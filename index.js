4a5c139759d5b60b3db5f74ae00f0b27ae3b7ab6
Roberto Ochoa <oc_005@hotmail.com>
2023-06-18 13:02:26 -0300

    port deploy

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const app = require("./src/app");
+const PORT = process.env.PORT || 3977;
 
-app.listen(3001, () => {
-  console.log("Server raised at port 3001");
+app.listen(PORT, () => {
+  console.log(`Server raised at port 3001 ${PORT}`);
 });
