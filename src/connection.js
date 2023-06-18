f9328d33a670066a67413514dba3af5b8aa20c8e
Roberto Ochoa <oc_005@hotmail.com>
2023-06-18 13:47:40 -0300

    control conexion

--- src/connection.js
+++ src/connection.js
@@ -1,17 +1,18 @@
+/*
 require("dotenv").config();
 const mysql = require("mysql2");
 const { HOST, USER, PASSWORD, DATABASE } = process.env;
 
 let connection;
 
-/* function handleDisconnect() { */
+/* function handleDisconnect() { 
 connection = mysql.createConnection({
   host: HOST,
   user: USER,
   password: PASSWORD,
   database: DATABASE,
-  /*     connectTimeout: 5000,
-    multipleStatements: true, */
+       connectTimeout: 5000,
+    multipleStatements: true,
 });
 
 /*   connection.connect((err) => {
@@ -30,6 +31,44 @@ handleDisconnect();
   connection.end(() => {
     handleDisconnect();
   });
-}); */
+}); 
+
+module.exports = connection;
+*/
+require("dotenv").config();
+const mysql = require("mysql2");
+const { HOST, USER, PASSWORD, DATABASE } = process.env;
+
+let connection;
+
+function handleDisconnect() {
+  connection = mysql.createConnection({
+    host: HOST,
+    user: USER,
+    password: PASSWORD,
+    database: DATABASE,
+  });
+
+  connection.connect((err) => {
+    if (err) {
+      console.error("Error al conectar a la base de datos:", err);
+      setTimeout(handleDisconnect, 200);
+    }
+    console.log("Conexión establecida");
+
+    // Realizar ping al servidor cada 4 segundos para mantener la conexión activa
+    setInterval(() => {
+      connection.ping((err) => {
+        if (err) {
+          console.error("Error en el ping de la base de datos:", err);
+          handleDisconnect(); // Reconectar si se produce un error en el ping
+        }
+        console.log("Ping enviado");
+      });
+    }, 4000);
+  });
+}
+
+handleDisconnect();
 
 module.exports = connection;
